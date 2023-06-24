447f6742cf79497ef4c39cf11147e4f75ecc5e86
Siva Allu <221910311060@gitam.in>
2023-06-25 03:53:33 +0530

    Voice To Text

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({ apiKey: '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'});
+const openai = new OpenAIApi(configuration);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/GPTresponse',async (req,res) => {
+try{
+  const response= await openai.createCompletion({
+    mode:'text-davinci-003',
+    prompt: req.body.text,
+    temperature: 0.7
+  })
+
+  //console.log(response);
+  res.json(response);
+} catch(error){
+  console.error(error);
+  res.status(500).json({error:"Something Went Wrong"});
+}
+
+
+})
 
 module.exports = router;
